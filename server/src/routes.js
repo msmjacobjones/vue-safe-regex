14fd55fd7eeb9cf0c12ba8f7060d7ae58f2103a7
jonsey247 <jacobfilmjones@hotmail.com>
2018-05-04 17:21:51 +0100

    first commit

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,5 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
-// const SongsController = require('./controllers/SongsController')
-// const BookmarksController = require('./controllers/BookmarksController')
-// const HistoriesController = require('./controllers/HistoriesController')
 
 const isAuthenticated = require('./policies/isAuthenticated')
 
